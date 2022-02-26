13f148b59a461391c89340c30a34b918a1f3e0af
Alok Malik <alok_malik@student.uml.edu>
2022-02-26 01:48:39 -0500

    online macpp

---
+++ macpp_online.py
@@ -0,0 +1,64 @@
+from macpp import MACPP, MACPPAgent
+import networkx as nx
+from collections import deque
+import numpy as np
+
+class MACPPOnline(MACPP):
+    def __init__(self, grid, num_directions):
+        super().__init__(grid, num_directions)
+        self.global_graph=self.graph
+        self.graph=nx.Graph()
+
+    def graph_constructor(self,crds,agent,depth):
+        '''
+        crds: oned coordinates of all agents
+        agent: the id of agent or the index of agent's coordinates in crds
+        depth: depth of bfs
+        '''
+        source=crds[agent]
+        queue=deque()
+        queue.append(source)
+        distances={}
+        distances[source]=0
+        nodes=[source]
+        while queue:
+            node=queue.popleft()
+            for neighbour in self.global_graph[node]:
+                if neighbour not in distances.keys() and distances[node]<depth:
+                    distances[neighbour]=distances[node]+1
+                    queue.append(neighbour)
+                    if neighbour not in nodes:
+                        nodes.append(neighbour)
+        for node in nodes:
+            if node not in self.graph:
+                self.graph.add_node(node,explored=0)
+                for neighbour in self.global_graph[node]:
+                    if neighbour in self.graph:
+                        self.graph.add_edge(node,neighbour)
+
+    def get_direction(self,crds,agent,depth):
+        '''
+        crds: a list containing coordinates of all agents
+        agent: index of agent in list crds
+        graph: the graph object of map
+        depth: depth of bfs
+        returns action preference scores of each direction
+        '''
+        self.graph_constructor(crds,agent,depth)
+        return super().get_direction(crds,agent,depth)
+
+
+
+    def mark(self,x,y,crds):
+        '''
+        input: cartesian coordinates of agent, and it's field of view
+        marks a cell visited or explored
+        '''
+        m=super().mark(x,y,crds)
+        x,y=self.convert(x,y)
+        source=self.n*x+y
+        if m:
+            self.global_graph.remove_node(source)
+        else:
+            self.global_graph.nodes[source]['explored']=self.explored_cell
+        return m
---
+++ run_macpp_online.py
@@ -0,0 +1,37 @@
+import numpy as np
+from macpp import MACPPAgent
+from macpp_online import MACPPOnline
+from maf import Grid
+import matplotlib.pyplot as plt
+import json
+from matplotlib import colors
+
+l,b=50,50
+grid=np.zeros((l,b))
+grid[int(l/2),:]=-1
+grid[int(l/2),int(b/2)]=0
+grid[int(l/2),int(b/2)+1]=0
+plt.imshow(grid)
+
+#g=Grid('new_map.json')
+#grid=g.return_grid()
+m=MACPPOnline(grid,4)
+agents=[]
+n=20
+x,y=m.convert(0,0)
+#print(m.n*x+y)
+crds=np.ones(n)*(m.n*x+y)
+for i in range(n):
+    agents.append(MACPPAgent(0,0,i,m))
+
+i=0
+while m.marked_visited()<1:
+    if agents[i].state():
+        crds[i]=agents[i].next(crds)
+    i+=1
+    i%=n
+    if i==0:
+        print(i,m.covered(),end=' | ')
+
+print('\n')
+print('map finished')

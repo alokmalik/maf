90253acd24af7b42a872d1dda049dbbfb47eb3fa
Alok Malik <alok_malik@student.uml.edu>
2022-07-06 01:20:28 -0400

    experiments

--- run_macpp_experiments.py
+++ run_macpp_experiments.py
@@ -4,19 +4,29 @@ from maf import Grid
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import os
 
-size=100
-filename='room_map_{}.json'.format(size)
+if not os.path.isdir('offline_experiments'):
+    os.mkdir('offline_experiments')
+
+size=50
+filename=0
+fname={}
+runs=10
 num_agents=20
-runs=5
+#edit the dictionary for file name of both maps
+fname[0]='room_map_{}.json'.format(size)
+fname[1]='new_room_map_{}.json'.format(size)
+
 data=np.zeros((num_agents,runs))
-g=Grid(filename)
+g=Grid(fname[filename])
 grid=g.return_grid()
 plt.imshow(grid)
 
 for r in tqdm(range(runs)):
+    visited=np.zeros(shape=(num_agents,num_agents))
     for n in tqdm(range(1,num_agents+1)):
-        g=Grid(filename)
+        g=Grid(fname[filename])
         grid=g.return_grid()
         m=MACPP(grid,4)
         agents=[]
@@ -31,18 +41,23 @@ for r in tqdm(range(runs)):
             agents.append(MACPPAgent(xc,yc,i,m))
         #print('agents made')
         i=0
-        count=[0 for _ in range(n)]
+        count=np.zeros(num_agents)
+
         while m.marked_visited()<1:
             if agents[i].state():
                 crds[i]=int(agents[i].next(crds))
                 count[i]+=1
+                visited[n-1,i]=agents[i].visited
+            else:
+                visited[n-1,i]=agents[i].visited
             i+=1
             i%=n
             #if i==0:
             #print(m.marked_visited(),end=' | ')
         data[n-1,r]=max(count)
-
+        rcols = ['agent_{}'.format(i) for i  in range(num_agents)]
+        rundf = pd.DataFrame(visited, columns = rcols)
+        rundf.to_csv('offline_experiments/offline_map_{}_size_{}_run_{}_visited.csv'.format(filename,size,r),index=False)
         cols=['Run {}'.format(i) for i in range(runs)]
-
         df = pd.DataFrame(data, columns = cols)
-        df.to_csv('results_macpp_{}.csv'.format(size),index=False)
+        df.to_csv('offline_experiments/results_offline_map_{}_size_{}.csv'.format(filename,size),index=False)
--- run_online_macpp_experiments.py
+++ run_online_macpp_experiments.py
@@ -5,18 +5,33 @@ from maf import Grid
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-size=100
-filename='room_map_{}.json'.format(size)
-num_agents=20
+import os
+
+#Edit the size of map
+size=50
+#edit the filename as per the dictionary
+filename=0
+fname={}
+#edit number of runs
 runs=10
+#max agents for our runs in 20
+num_agents=20
+#edit the dictionary for file name of both maps
+fname[0]='room_map_{}.json'.format(size)
+fname[1]='new_room_map_{}.json'.format(size)
+
+if not os.path.isdir('online_experiments'):
+    os.mkdir('online_experiments')
+
 data=np.zeros((num_agents,runs))
-g=Grid(filename)
+g=Grid(fname[filename])
 grid=g.return_grid()
 plt.imshow(grid)
 
 for r in tqdm(range(runs)):
+    visited=np.zeros(shape=(num_agents,num_agents))
     for n in tqdm(range(1,num_agents+1)):
-        g=Grid(filename)
+        g=Grid(fname[filename])
         grid=g.return_grid()
         m=MACPPOnline(grid,4)
         agents=[]
@@ -36,12 +51,21 @@ for r in tqdm(range(runs)):
             if agents[i].state():
                 crds[i]=int(agents[i].next(crds))
                 count[i]+=1
+                visited[n-1,i]=agents[i].visited
+            else:
+                visited[n-1,i]=agents[i].visited
             i+=1
             i%=n
             #if i==0:
             #print(m.marked_visited(),end=' | ')
+
+        
+        rcols = ['agent_{}'.format(i) for i  in range(num_agents)]
+        rundf = pd.DataFrame(visited, columns = rcols)
+        rundf.to_csv('online_experiments/online_map_{}_size_{}_run_{}_visited.csv'.format(filename,size,r),index=False)
+
         data[n-1,r]=max(count)
         cols=['Run {}'.format(i) for i in range(runs)]
 
         df = pd.DataFrame(data, columns = cols)
-        df.to_csv('results_onlinemacpp_{}.csv'.format(size),index=False)
+        df.to_csv('online_experiments/results_online_map_{}_size_{}.csv'.format(filename,size),index=False)
